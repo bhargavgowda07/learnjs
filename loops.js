968729e25ac2db3a68a650b76e4c0ac59d31f8aa
bhargav <bhargav@example.com>
2025-06-08 19:30:29 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,56 @@
+/*for(i=1; i<5; i++){
+
+    console.log("bhargav")
+    console.log("vagaggag")
+}
+
+
+i=10
+
+while(i>7){
+    console.log("mmmmm")
+    console.log("vvvvv")
+    i--
+}
+
+
+//difference b/w do while and while loop is dowhile execute atleast once whether if condition false also
+
+i = 10
+
+do {
+    console.log(i)
+    console.log("mmmmm")
+    console.log("vvvvv")
+    i--
+} while (i > 1) {
+
+}*/
+
+for (i = 1; i < 10; i++) {
+    console.log(i)
+    console.log("bhargav")
+    console.log("b r")
+}
+
+
+
+i=1
+
+while(i<10){
+    console.log(i)
+    console.log("bhargav")
+    console.log("b rrrrrrrrr")
+    i++
+}
+
+
+i=5
+
+do{
+       console.log(i)
+    console.log("vvvvvv")
+    console.log("mmmmr")
+    i--
+}while(i>1)
+{}
